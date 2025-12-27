effe9f1ce1a135f277a102c4efe7eefc3b4c9db2
LimDongHwi <51016978+lZoal@users.noreply.github.com>
2025-12-27 09:06:23 +0900

    fix scripts

---
+++ src/evolve_automation.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Evolve
+// @namespace    http://tampermonkey.net/
+// @version      3.3.1.147
+// @description  try to take over the world!
+// @downloadURL  https://github.com/Vollch/Evolve-Automation/raw/master/evolve_automation.user.js
+// @updateURL    https://github.com/Vollch/Evolve-Automation/raw/master/evolve_automation.meta.js
+// @author       Fafnir
+// @author       TMVictor
+// @author       Vollch
+// @author       schoeggu
+// @author       davezatch
+// @author       Kewne
+// @author       SkyeAmphi
+// @match        https://pmotschmann.github.io/Evolve/
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
+// @require      https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
+// ==/UserScript==
